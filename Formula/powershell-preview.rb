c332c64c1d396f9a7bcee403820198dda30e07b2
Travis Plunk <github@ez13.net>
2020-04-17 11:47:54 -0700

    Add preview formula (#4)
    * reflect rename of repo
    
    * Add version manually
    
    * update workflows
    
    * add new formula
    
    * give github actions meaningful names

--- Formula/powershell-preview.rb
+++ Formula/powershell-preview.rb
@@ -1,13 +1,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-class Powershell < Formula
-  desc "PowerShell"
+class PowershellPreview < Formula
+  desc "PowerShell Preview"
   homepage "https://github.com/powershell/powershell"
   # We do not specify `version "..."` as 'brew audit' will complain - see https://github.com/Homebrew/legacy-homebrew/issues/32540
-  url "https://github.com/PowerShell/PowerShell/releases/download/v7.0.0/powershell-7.0.0-osx-x64.tar.gz"
+  url "https://github.com/PowerShell/PowerShell/releases/download/v7.1.0-preview.1/powershell-7.1.0-preview.1-osx-x64.tar.gz"
   # must be lower-case
-  sha256 "7ea2a539cb33f3c1c62280eea1d3b55cbd84c86676437a390e81c0ae374483e6"
-  version_scheme "7.0.0"
+  sha256 "c6903d6e765c7a889a497f915321a187c0e43bf3d5d06bcb8ff45dae18a35445"
+  version_scheme "7.0.0-preview.1"
   bottle :unneeded
 
   # .NET Core 3.1 requires High Sierra - https://docs.microsoft.com/en-us/dotnet/core/install/dependencies?pivots=os-macos&tabs=netcore31
@@ -16,27 +16,28 @@ class Powershell < Formula
   def install
     libexec.install Dir["*"]
     chmod 0555, libexec/"pwsh"
-    bin.install_symlink libexec/"pwsh"
+    bin.install_symlink libexec/"pwsh" => "pwsh-preview"
   end
 
   def caveats
     <<~EOS
-      The executable should already be on PATH so run with `pwsh`. If not, the full path to the executable is:
-        #{bin}/pwsh
+      The executable should already be on PATH so run with `pwsh-preview`. If not, the full path to the executable is:
+        #{bin}/pwsh-preview
 
       Other application files were installed at:
         #{libexec}
 
       If you also have the Cask installed, you need to run the following to make the formula your default install:
-        brew link --overwrite powershell
+        brew link --overwrite powershell-preview
 
       If you would like to make PowerShell you shell, run
-        sudo echo '#{bin}/pwsh' >> /etc/shells
-        chsh -s #{bin}/pwsh
+        sudo echo '#{bin}/pwsh-preview' >> /etc/shells
+        chsh -s #{bin}/pwsh-preview
     EOS
   end
 
   test do
-    assert_equal "7.0.0", shell_output("#{bin}/pwsh -c '$psversiontable.psversion.tostring()'").strip
+    assert_equal "7.1.0-preview.1",
+      shell_output("#{bin}/pwsh-preview -c '$psversiontable.psversion.tostring()'").strip
   end
 end
